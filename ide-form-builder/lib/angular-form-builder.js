7c6295c1e865ecf3f2e3b10c906f147770265266
Nedelcho Delchev <delchevn@gmail.com>
2021-09-25 13:07:35 +0300

    Update angular-form-builder.js

--- ide-form-builder/lib/angular-form-builder.js
+++ ide-form-builder/lib/angular-form-builder.js
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
 (function() {
   var copyObjectToScope;
   var $ = jQuery;
@@ -1095,14 +1106,14 @@
         $http.get(component.templateUrl, {
           cache: $templateCache
         }).then(function(template) {
-          return component.template = template;
+          return component.template = template.data;
         });
       }
       if (component.popoverTemplate == null) {
         return $http.get(component.popoverTemplateUrl, {
           cache: $templateCache
         }).then(function(template) {
-          return component.popoverTemplate = template;
+          return component.popoverTemplate = template.data;
         });
       }
     };
